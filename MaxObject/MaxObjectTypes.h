ae927114d2eec32179fc341c7944c1024c37e7da
Timothy Place <tim@electrotap.com>
2009-10-29 17:48:32 -0500

    SymbolGen() template now compiles on the Mac with no compiler errors.

--- MaxObject/MaxObjectTypes.h
+++ MaxObject/MaxObjectTypes.h
@@ -29,7 +29,13 @@ typedef t_hashtab*	HashtabPtr;
 
 //SymbolPtr SymbolGen(const char* string);	// temporarilty implemented in Jamoma.cpp
 											// use this instead of gensym() so that we don't have tons of const warnings
-template <>
+template <class T>
+SymbolPtr SymbolGen(T stringArg)
+{
+	return gensym((char*)stringArg);
+}
+
+template <class T>
 SymbolPtr SymbolGen(const char* stringArg)
 {
 	return gensym((char*)stringArg);
